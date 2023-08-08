fc5a463b9bc84542908d0dae595765fe94a2f25b
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-08 11:50:19 +0800

    Update section2-5.cpp

--- an/chapater2/section2-5.cpp
+++ an/chapater2/section2-5.cpp
@@ -2,7 +2,7 @@
  * @Author: LynnGao
  * @Date: 2023-08-08 10:04:16
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-08 11:34:51
+ * @LastEditTime: 2023-08-08 11:50:11
  * @Description: 数组 page39-54
  * 数组:相同数据类型的变量组合在一起而产生的数据集合
  * @FilePath: \learnsomething\an\chapater2\section2-5.cpp
@@ -15,10 +15,26 @@
 
 #include <stdio.h>
 
-int main(){
-    int a[10] = {5, 3, 2, 6, 8, 4};
-    for(int i = 0; i < 10; i++){
-        printf("a[%d] = %d\n", i, a[i]);
-    }
-    return 0;
-}
+// int main(){
+//     int a[10] = {5, 3, 2, 6, 8, 4};
+//     for(int i = 0; i < 10; i++){
+//         printf("a[%d] = %d\n", i, a[i]);
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int a[10];
+//     scanf("%d", &a[0]);
+//     for(int i = 1; i < 10; i++){
+//         a[i] = a[i - 1] * 2;
+//     }
+//     for(int i = 0; i < 10; i++){
+//         printf("a[%d] = %d\n", i, a[i]);
+//     }
+//     return 0;
+// }
+
+// 2.5.2 冒泡排序 page 41-43
+
+// 2.5.3 二维数组 page 43-46
