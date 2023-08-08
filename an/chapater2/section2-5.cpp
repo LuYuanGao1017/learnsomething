e21b69d3d59f50a5e16636e71aea2f464241d269
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-08 15:48:22 +0800

    Update section2-5.cpp

--- an/chapater2/section2-5.cpp
+++ an/chapater2/section2-5.cpp
@@ -2,7 +2,7 @@
  * @Author: LynnGao
  * @Date: 2023-08-08 10:04:16
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-08 11:50:11
+ * @LastEditTime: 2023-08-08 15:47:14
  * @Description: 数组 page39-54
  * 数组:相同数据类型的变量组合在一起而产生的数据集合
  * @FilePath: \learnsomething\an\chapater2\section2-5.cpp
@@ -37,4 +37,85 @@
 
 // 2.5.2 冒泡排序 page 41-43
 
+// 1. 交换两个数
+
+// int main(){
+//     int a = 1, b = 2;
+//     printf("a = %d\t b = %d\n", a, b);
+//     printf("After exchange!\n");
+//     int temp = a;
+//     a = b;
+//     b = temp;
+//     printf("a = %d\t b = %d\n", a, b);
+//     return 0;
+// }
+
+// 2.冒泡排序
+// page 43
+
+// int main(){
+//     int a[10] = {3, 1, 4, 5, 2};
+//         for(int i = 0; i < 5; i++){
+//         printf("a[%d] = %d\t", i, a[i]);
+//     }
+//     printf("\nAfter bubble\n");
+//     for(int i = 1; i <=4; i++){
+//         for(int j = 0; j < 5 - i; j++){
+//             if(a[j] > a[j + 1]){
+//                 int temp = a[j];
+//                 a[j] = a[j + 1];
+//                 a[j + 1] = temp;
+//             }
+//         }
+//     }
+//     for(int i = 0; i < 5; i++){
+//         printf("a[%d] = %d\t", i, a[i]);
+//     }
+//     return 0;
+// }
+
 // 2.5.3 二维数组 page 43-46
+
+// int main(){
+//     int a[5][6] = {{3, 1, 2}, {8, 4}, {}, {1, 2, 3, 4, 5}};
+//     for (int i = 0; i < 5; i++)
+//     {
+//         for (int j = 0; j < 6; j++)
+//         {
+//             printf("%d\t", a[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+int main(){
+    int a[3][3], b[3][3];
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    int c[3][3];
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    
+    for(int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
