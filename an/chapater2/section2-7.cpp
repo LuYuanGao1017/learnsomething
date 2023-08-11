94faec61026c78707a1785e00158385e977ea46f
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-11 19:10:29 +0800

    Update section2-7.cpp

--- an/chapater2/section2-7.cpp
+++ an/chapater2/section2-7.cpp
@@ -2,9 +2,15 @@
  * @Author: LynnGao
  * @Date: 2023-08-09 11:47:54
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-11 16:42:10
+ * @LastEditTime: 2023-08-11 17:16:44
  * @Description: 指针pointer
  * @FilePath: \learnsomething\an\chapater2\section2-7.cpp
  */
 
 #include <stdio.h>
+
+int main(){
+    int a = 1;
+    printf("%d, %d\n", &a, a);
+    return 0;
+}
