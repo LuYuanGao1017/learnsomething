4f61ab09e4782ce9a635d3adb139e3888d819a60
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-11 16:41:59 +0800

    Update section2-6.cpp

--- an/chapater2/section2-6.cpp
+++ an/chapater2/section2-6.cpp
@@ -103,23 +103,36 @@
 
 // 2.6.3 以数组作为函数参数
 
-void change(int a[], int b[][5]){
-    a[0] = 1;
-    a[1] = 3;
-    a[2] = 5;
-    b[0][0] = 1;
-}
+// void change(int a[], int b[][5]){
+//     a[0] = 1;
+//     a[1] = 3;
+//     a[2] = 5;
+//     b[0][0] = 1;
+// }
 
-int main(){
-    int a[3] = {0};
-    int b[5][5] = {0};
-    change(a, b);
-    for (int i = 0; i < 3; i++)
-        printf("%d\n", a[i]);
-    return 0;
-}
+// int main(){
+//     int a[3] = {0};
+//     int b[5][5] = {0};
+//     change(a, b);
+//     for (int i = 0; i < 3; i++)
+//         printf("%d\n", a[i]);
+//     return 0;
+// }
 
 // 2.6.4 函数的嵌套调用
 
 // 2.6.5 函数的递归调用
 
+// 递归 函数自己调用自己的过程
+
+int F(int n){
+    if(n == 0) return 1;
+    else return F(n - 1) * n;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", F(n));
+    return 0;
+}
