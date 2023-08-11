4d8944de11b40db1faf98e6ff91fbb71b5c4248c
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-11 19:15:55 +0800

    Update section2-7.cpp

--- an/chapater2/section2-7.cpp
+++ an/chapater2/section2-7.cpp
@@ -2,15 +2,17 @@
  * @Author: LynnGao
  * @Date: 2023-08-09 11:47:54
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-11 17:16:44
+ * @LastEditTime: 2023-08-11 19:13:47
  * @Description: 指针pointer
  * @FilePath: \learnsomething\an\chapater2\section2-7.cpp
  */
 
 #include <stdio.h>
-
+// 2.7.1 什么是指针
+// 变量的地址
 int main(){
     int a = 1;
     printf("%d, %d\n", &a, a);
     return 0;
 }
+
