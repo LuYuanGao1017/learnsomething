25935defdea96cca5655bd8a21c98ccee7a02ea0
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-12 16:10:17 +0800

    Create supplement.cpp

---
+++ an/chapater2/supplement.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: LynnGao
+ * @Date: 2023-08-12 16:04:59
+ * @LastEditors: LynnGao
+ * @LastEditTime: 2023-08-12 16:09:46
+ * @Description: C++中的cin cout 浮点数比较 复杂度
+ * @FilePath: \learnsomething\an\chapater2\supplement.cpp
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    cout << n;
+    return 0;
+}
