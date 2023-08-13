ffe61f6ea541cb5dd6a2c567b53446e4e6a3386f
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-13 15:48:17 +0800

    Update blackboxTest.cpp

--- an/chapater2/blackboxTest.cpp
+++ an/chapater2/blackboxTest.cpp
@@ -2,11 +2,33 @@
  * @Author: LynnGao
  * @Date: 2023-08-12 16:06:06
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-13 15:14:44
+ * @LastEditTime: 2023-08-13 15:35:22
  * @Description: 黑盒测试
  * @FilePath: \learnsomething\an\chapater2\blackboxTest.cpp
  */
 
+// 根据黑盒测试是否对每组测试数据
+// 都单独测试
+// 或是一次性测试所有测试数据 
+// 分为
+
+#include <stdio.h>
+
 // 2.10.1 单点测试
+// 判断每组数据的输出结果是否正确
+
+// int main(){
+//     int a, b;
+//     scanf("%d%d", &a, &b);
+//     printf("%d\n", a + b);
+//     return 0;
+// }
 
 // 2.10.2 多点测试
+// 3种输入方式
+// (1) while...EOF
+
+
+// (2) while break
+
+// (3) while(T--)
