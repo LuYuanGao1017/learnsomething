c8ba8ac321093f443157c0d2492e12349ef04b14
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-13 19:52:18 +0800

    Update blackboxTest.cpp

--- an/chapater2/blackboxTest.cpp
+++ an/chapater2/blackboxTest.cpp
@@ -2,7 +2,7 @@
  * @Author: LynnGao
  * @Date: 2023-08-12 16:06:06
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-13 15:35:22
+ * @LastEditTime: 2023-08-13 19:47:48
  * @Description: 黑盒测试
  * @FilePath: \learnsomething\an\chapater2\blackboxTest.cpp
  */
@@ -27,8 +27,37 @@
 // 2.10.2 多点测试
 // 3种输入方式
 // (1) while...EOF
+// 如果题目没给输入的结束条件 默认读取文件到末尾 
+// C语言使用EOF End of File 来代表-1
 
+// int main(){
+//     int a, b;
+//     while(scanf("%d%d", &a, &b) != EOF)
+//         printf("%d\n", a + b);
+//     return 0;
+// }
+
+// Terminal输入数据时不会触发EOF状态 手动触发 ctrl+z
+
+// while(gets(str) != NULL)
 
 // (2) while break
+// 满足某个条件时退出中断
 
+int main(){
+    int a, b;
+    while(scanf("%d%d", &a, &b) != EOF){
+        if(a == 0 && b == 0) break;
+        printf("%d\n", a + b);
+    }
+    return 0;
+}
+
+// 简洁写法
+int main(){
+    int a, b;
+    while (scanf("%d%d", &a, &b) != EOF, a || b)
+        printf("%d\n", a + b);
+    
+}
 // (3) while(T--)
