6e2f34194409245fd51d188d7905f18793427526
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-13 15:33:50 +0800

    Update blackboxTest.cpp

---
+++ an/chapater2/blackboxTest.cpp
@@ -0,0 +1,12 @@
+/*
+ * @Author: LynnGao
+ * @Date: 2023-08-12 16:06:06
+ * @LastEditors: LynnGao
+ * @LastEditTime: 2023-08-13 15:14:44
+ * @Description: 黑盒测试
+ * @FilePath: \learnsomething\an\chapater2\blackboxTest.cpp
+ */
+
+// 2.10.1 单点测试
+
+// 2.10.2 多点测试
