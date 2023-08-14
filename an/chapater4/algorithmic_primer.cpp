2318c03a385420a8b505780bee474d7618b73487
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-14 16:02:47 +0800

    Update algorithmic_primer.cpp

--- an/chapater4/algorithmic_primer.cpp
+++ an/chapater4/algorithmic_primer.cpp
@@ -2,7 +2,7 @@
  * @Author: LynnGao
  * @Date: 2023-08-09 11:49:36
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-14 15:40:21
+ * @LastEditTime: 2023-08-14 15:56:17
  * @Description:
  * @FilePath: \learnsomething\an\chapater4\algorithmic_primer.cpp
  */
@@ -128,26 +128,27 @@ using namespace std;
 // }
 
 // 2.M个整数在N个整数中出现次数
-const int maxn = 10010;
-int hashTable[maxn] = {0};
+// const int maxn = 10010;
+// int hashTable[maxn] = {0};
 
-int main()
-{
-    int n, m, x;
-    scanf("%d%d", &n, &m);
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &x);
-        hashTable[x]++;
-    }
-    for (int i = 0; i < m; i++)
-    {
-        scanf("%d", &x);
-        printf("%d ", hashTable[x]);
-    }
+// int main()
+// {
+//     int n, m, x;
+//     scanf("%d%d", &n, &m);
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &x);
+//         hashTable[x]++;
+//     }
+//     for (int i = 0; i < m; i++)
+//     {
+//         scanf("%d", &x);
+//         printf("%d ", hashTable[x]);
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
 // 上面两个直接把输入的数作为下标
 // 以空间换时间的方法 且每个数不超过10^5
 
@@ -155,19 +156,81 @@ int main()
 // 散列能把这些转换为一个能接受范围内的整数
 
 // 4.2.1 散列的定义与整数散列
-//  散列函数 
+//  散列函数
 
 // 4.2.2 字符串hash初步
 
-// 假设字符串由A~Z组成 0~25 26^len - 1最大的整数 len为字符串长度
-int hashFunc(char s[], int len){
+// // 假设字符串由A~Z组成 0~25 26^len - 1最大的整数 len为字符串长度
+// int hashFunc(char s[], int len)
+// {
+//     int id = 0;
+//     for (int i = 0; i < len; i++)
+//         id = id * 26 + (s[i] - 'A'); // 将26进制转换为十进制
+//     return id;
+// }
+// // 如果字符串s长度比较长那么换成整数也会很大
+
+// int hashFunc(char s[], int len)
+// {
+//     int id = 0;
+//     for (int i = 0; i < len; i++)
+//     {
+//         if (s[i] >= 'A' && s[i] <= 'Z')
+//         {
+//             id = id * 52 + (s[i] - 'A');
+//         }
+//         else if (s[i] >= 'a' && s[i] <= 'z')
+//         {
+//             id = id * 52 + (s[i] - 'a') + 26;
+//         }
+//     }
+//     return id;
+// }
+
+// 给出N个字符串 再给出M个查询字符串 问每个查询字符串在N个字符串出现的次数
+
+const int maxn = 100;
+char s[maxn][5], temp[5];
+int hashTable[26 * 26 * 26 + 10]; // 定义合适大小的数组
+
+int hashFunc(char s[], int len)
+{
     int id = 0;
-    for (int i = 0; i < len; i++) id = id * 26 + (s[i] - 'A');  //将26进制转换为十进制
+    for (int i = 0; i < len; i++)
+    {
+        id = id * 26 + (s[i] - 'A');
+    }
     return id;
 }
-// 如果字符串s长度比较长那么换成整数也会很大
 
+int main()
+{
+    int n, m;
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%s", s[i]);
+        int id = hashFunc(s[i], 3);
+        hashTable[id]++;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%s", temp);
+        int id = hashFunc(temp, 3);
+        printf("%d\n", hashTable[id]);
+    }
+    return 0;
+}
 
 // 4.3 递归
 
 // 4.3.1 分治
+
+// 4.3.2 递归
+
+// 4.4 贪心
+
+// 4.4.1 简单贪心
+
+// 4.4.2 区间贪心
