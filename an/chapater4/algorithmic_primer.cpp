8da4308b446cf426d875aadade33ce23ac68981e
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-14 14:29:53 +0800

    Update algorithmic_primer.cpp

--- an/chapater4/algorithmic_primer.cpp
+++ an/chapater4/algorithmic_primer.cpp
@@ -2,7 +2,7 @@
  * @Author: LynnGao
  * @Date: 2023-08-09 11:49:36
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-14 13:29:40
+ * @LastEditTime: 2023-08-14 14:07:34
  * @Description:
  * @FilePath: \learnsomething\an\chapater4\algorithmic_primer.cpp
  */
@@ -10,6 +10,8 @@
 #include <stdio.h>
 #include <algorithm>
 #include <string.h>
+using namespace std;
+
 // 4.1　排序
 // 4.1.1
 // 4.1.2
@@ -48,3 +50,45 @@
 //     }
 //     return 0;
 // }
+
+// PAT A1025
+struct testee
+{
+    char id[15];
+    int score;
+    int location_num;
+    int local_rank;
+} stu[300];
+
+bool cmp(testee a, testee b)
+{
+    if (a.score != b.score)
+        return a.score > b.score;
+    else
+        return strcmp(a.id, b.id) < 0;
+}
+
+int main()
+{
+    int n, k, num = 0;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &k);
+        for (int j = 0; j < k; j++)
+        {
+            scanf("%s %d", stu[num].id, &stu[num].score);
+            stu[num].location_num = i;
+            num++;
+        }
+        sort(stu + num - k, stu + num, cmp);
+        stu[num - k].location_num = 1;
+        for(int j = num - k + 1; j < num; j++){
+            if(stu[j].score == stu[j - 1].score){
+                stu[j].local_rank = stu[j - 1].local_rank;
+            }else{
+                stu[j].local_rank = j + 1 - (num - k);
+            }
+        }
+    }
+}
