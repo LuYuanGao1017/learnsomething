738bf34394d4812087e4b4a2c2b8b170c29e8d6e
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-16 15:08:51 +0800

    Update binary.cpp

--- an/chapater4/binary.cpp
+++ an/chapater4/binary.cpp
@@ -2,12 +2,13 @@
  * @Author: LynnGao
  * @Date: 2023-08-16 13:40:30
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-16 14:26:55
+ * @LastEditTime: 2023-08-16 14:49:34
  * @Description: 基于有序序列的二分查找
  * @FilePath: \learnsomething\an\chapater4\binary.cpp
  */
 
 #include <stdio.h>
+#include <math.h>
 
 // 4.5.1二分查找
 
@@ -34,29 +35,49 @@
 // }
 
 // 4.5.2 二分法拓展
-#include <math.h>
+
+// 给定单调函数f(x)求f(x)=0的根
+// const double eps = 1e-5;
+
+// double f(double x)
+// {
+//     return x * x;
+// }
+
+// double calSqrt()
+// {
+//     double left = 1, right = 2, mid;
+//     while (right - left > eps)
+//     {
+//         mid = (left + right) / 2;
+//         if (f(mid) > 2)
+//             right = mid;
+//         else
+//             left = mid;
+//     }
+//     return mid;
+// }
+
+// int main(){
+//     printf("%.3f", calSqrt());
+//     return 0;
+// }
+
+// 装水问题
+
+const double PI = acos(-1.0);
 const double eps = 1e-5;
 
-double f(double x)
+double f(double R, double h)
 {
-    return x * x;
-}
-
-double calSqrt()
-{
-    double left = 1, right = 2, mid;
-    while (right - left > eps)
-    {
-        mid = (left + right) / 2;
-        if (f(mid) > 2)
-            right = mid;
-        else
-            left = mid;
-    }
-    return mid;
+    double alpha = 2 * acos((R - h) / R);
+    double X = 2 * sqrt(R * R - (R - h) * (R - h));
+    double S1 = alpha * R * R / 2 - X * (R - h) / 2;
+    double S2 = PI * R * R / 2;
+    return S1 / S2;
 }
 
 int main(){
-    printf("%.3f", calSqrt());
+    printf("%lf", PI);
     return 0;
 }
