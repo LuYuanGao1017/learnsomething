2fc9e9c70aab38ffa3f4df7c8d85ad8d666cbc2e
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-19 17:28:33 +0800

    greatest common divisor and least common multiple

--- an/chapater5/section2.cpp
+++ an/chapater5/section2.cpp
@@ -2,7 +2,7 @@
  * @Author: LynnGao
  * @Date: 2023-08-19 16:55:39
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-19 17:27:09
+ * @LastEditTime: 2023-08-19 17:28:26
  * @Description: greatest common divisor and least common multiple
  * @FilePath: \learnsomething\an\chapater5\section2.cpp
  */
@@ -18,18 +18,19 @@ int gcd(int a, int b)
         return gcd(b, a % b);
 }
 
-// int main()
-// {
-//     int m, n;
-//     while (scanf("%d%d", &m, &n) != EOF)
-//         printf("%d\n", gcd(m, n));
+int main()
+{
+    int m, n;
+    while (scanf("%d%d", &m, &n) != EOF)
+        printf("%d\n", gcd(m, n));
 
-//     return 0;
-// }
+    return 0;
+}
 
 // 5.2.2最小公倍数
 
-int lcm(int a, int b){
+int lcm(int a, int b)
+{
     return (a * b) / gcd(a, b);
 }
 
