98f573cfd00dfb4c3da4c4bf8f02b7c5d41ecdaf
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-20 18:25:25 +0800

    Create prime_factor_decomposition.cpp

---
+++ an/chapater5/prime_factor_decomposition.cpp
@@ -0,0 +1,8 @@
+/*
+ * @Author: LynnGao
+ * @Date: 2023-08-20 17:31:33
+ * @LastEditors: LynnGao
+ * @LastEditTime: 2023-08-20 17:31:43
+ * @Description: 
+ * @FilePath: \learnsomething\an\chapater5\prime_factor_decomposition.cpp
+ */
