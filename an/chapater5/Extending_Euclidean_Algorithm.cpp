07bed6a49b2c7b36234bcf1e7058f42f4518242a
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-21 21:38:22 +0800

    Update Extending_Euclidean_Algorithm.cpp

---
+++ an/chapater5/Extending_Euclidean_Algorithm.cpp
@@ -0,0 +1,12 @@
+/*
+ * @Author: LynnGao
+ * @Date: 2023-08-21 14:59:55
+ * @LastEditors: LynnGao
+ * @LastEditTime: 2023-08-21 15:05:30
+ * @Description:
+ * 1. ax + by = gcd(a, b) 求解
+ * 2. ax + by = c 求解
+ * 3. 同余式ax = c(mod m) 求解
+ * 4. 逆元的求解 以及 (b / a) % m 计算
+ * @FilePath: \learnsomething\an\chapater5\Extending_Euclidean_Algorithm.cpp
+ */
