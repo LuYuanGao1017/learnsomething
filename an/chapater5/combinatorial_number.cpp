4bd686bb124eefe0da36eea640b43ac136cbc05e
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-21 21:38:04 +0800

    Create combinatorial_number.cpp

---
+++ an/chapater5/combinatorial_number.cpp
@@ -0,0 +1,9 @@
+/*
+ * @Author: LynnGao
+ * @Date: 2023-08-21 21:37:39
+ * @LastEditors: LynnGao
+ * @LastEditTime: 2023-08-21 21:37:48
+ * @Description: 
+ * @FilePath: \learnsomething\an\chapater5\combinatorial_number.cpp
+ */
+// 5.8.1 n!
