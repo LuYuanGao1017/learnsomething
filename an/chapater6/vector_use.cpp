4f5222e5641fe97b8ef99d64c9715a1a6ccc16a7
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-25 09:26:36 +0800

    Update vector_use.cpp

--- an/chapater6/vector_use.cpp
+++ an/chapater6/vector_use.cpp
@@ -2,7 +2,7 @@
  * @Author: LynnGao
  * @Date: 2023-08-21 21:40:36
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-25 08:42:54
+ * @LastEditTime: 2023-08-25 09:21:32
  * @Description: 变长数组 以邻接表的方式存储图
  * @FilePath: \learnsomething\an\chapater6\vector_use.cpp
  */
@@ -92,19 +92,44 @@ using namespace std;
 
 // (4) clear()
 // 清空vector中所有元素
-int main()
-{
-    vector<int> vi;
-    for (int i = 1; i <= 3; i++)
-        vi.push_back(i);
-    vi.clear();
-    printf("%d\n", vi.size());
-    return 0;
-}
+
+// int main()
+// {
+//     vector<int> vi;
+//     for (int i = 1; i <= 3; i++)
+//         vi.push_back(i);
+//     vi.clear();
+//     printf("%d\n", vi.size());
+//     return 0;
+// }
+
 // (5) insert()
 // 任意迭代器It处插入一个元素 O(N)
 
+// int main()
+// {
+//     vector<int> vi;
+//     for (int i = 1; i <= 5; i++)
+//         vi.push_back(i);
+//     vi.insert(vi.begin() + 2, -1);
+//     for (int i = 0; i < vi.size(); i++)
+//         printf("%d ", vi[i]);
+//     return 0;
+// }
+
 // (6) erase()
+// 删除单个元素 删除一个区间的所有元素 O(N)
+int main()
+{
+    vector<int> vi;
+    for (int i = 5; i <= 9; i++)
+        vi.push_back(i); // 5 6 7 8 9
+    // vi.begin()对应的是vi[0] 
+    vi.erase(vi.begin() + 3);
+    for (int i = 0; i < vi.size(); i++)
+        printf("%d ", vi[i]); // 5 6 7 9
+    return 0;
+}
 
 // 4.vector常见用途
 // (1) 储存数据
