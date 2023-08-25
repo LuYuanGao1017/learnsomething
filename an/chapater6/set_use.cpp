d3b981fbe2239f9b17683cae4f74c33da49fa2b9
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-25 16:26:34 +0800

    Update set_use.cpp

--- an/chapater6/set_use.cpp
+++ an/chapater6/set_use.cpp
@@ -2,7 +2,7 @@
  * @Author: LynnGao
  * @Date: 2023-08-23 13:51:25
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-25 16:23:13
+ * @LastEditTime: 2023-08-25 16:25:38
  * @Description:
  * set集合
  * 内部自动有序 不含重复元素的容器
@@ -21,23 +21,33 @@ using namespace std;
 // 只能通过迭代器访问
 // ste<typename>::iterator it;
 
-int main()
-{
-    set<int> st;
-    st.insert(3);
-    st.insert(5);
-    st.insert(2);
-    st.insert(3);
-    for (set<int>::iterator it = st.begin(); it != st.end(); it++)
-        printf("%d ", *it);
+// int main()
+// {
+//     set<int> st;
+//     st.insert(3);
+//     st.insert(5);
+//     st.insert(2);
+//     st.insert(3);
+//     for (set<int>::iterator it = st.begin(); it != st.end(); it++)
+//         printf("%d ", *it);
 
-    return 0;
-}
+//     return 0;
+// }
 
 // 3.set常用函数实例解析
 // (1) insert() 自动递增排序并去重
 // (2) find(value) 返回set中对应值为value的迭代器 O(logN) N为set内元素个数
 
+int main()
+{
+    set<int> st;
+    for (int i = 1; i <= 3; i++)
+        st.insert(i);
+    set<int>::iterator it = st.find(2);
+    printf("%d\n", *it);
+    // printf("%d\n", *(st.find(2)));
+    return 0;
+}
 
 // 4.set常见用途
 // 自动去重并按升序排序
