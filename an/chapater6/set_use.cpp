7a7c451c355cc23bb843ecef376b06cf6f9708a9
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-25 16:13:01 +0800

    Create set_use.cpp

---
+++ an/chapater6/set_use.cpp
@@ -0,0 +1,40 @@
+/*
+ * @Author: LynnGao
+ * @Date: 2023-08-23 13:51:25
+ * @LastEditors: LynnGao
+ * @LastEditTime: 2023-08-25 16:11:52
+ * @Description:
+ * set集合
+ * 内部自动有序 不含重复元素的容器
+ * 去掉重复元素的情况比较大或者类型不是Int型而不能直接开散列表
+ * @FilePath: \learnsomething\an\chapater6\set_use.cpp
+ */
+
+#include <stdio.h>
+#include <set>
+using namespace std;
+
+// 1.定义set
+// set<typename> name;
+
+// 2.set容器内元素的访问
+// 只能通过迭代器访问
+// ste<typename>::iterator it;
+
+int main()
+{
+    set<int> st;
+    st.insert(3);
+    st.insert(5);
+    st.insert(2);
+    st.insert(3);
+    for (set<int>::iterator it = st.begin(); it != st.end(); it++)
+        printf("%d ", *it);
+
+    return 0;
+}
+
+// 3.set常用函数实例解析
+
+// 4.set常见用途
+// 自动去重并按升序排序
