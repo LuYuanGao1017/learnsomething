0150809deff1a94e9096165658f97820e86a35de
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-26 16:27:33 +0800

    Update queue_use.cpp

--- an/chapater6/queue_use.cpp
+++ an/chapater6/queue_use.cpp
@@ -2,7 +2,7 @@
  * @Author: LynnGao
  * @Date: 2023-08-26 16:16:41
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-26 16:16:50
+ * @LastEditTime: 2023-08-26 16:25:24
  * @Description:
  * 先进先出 队列
  * @FilePath: \learnsomething\an\chapater6\queue_use.cpp
@@ -15,13 +15,26 @@ using namespace std;
 // 1. queue的定义
 // 2. queue容器内元素的访问
 
+// int main()
+// {
+//     queue<int> q;
+//     for (int i = 1; i <= 5; i++)
+//         q.push(i);
+//     printf("%d %d\n", q.front(), q.back());
+//     return 0;
+// }
+
+// 3.queue常用函数实例
+// (1) push(x) 将 x 入列 O(1)
+// (2) front() back() 分别获得队首元素和队尾元素 O(1)
+// (3) pop() 队首元素出队 O(1)
 int main()
 {
     queue<int> q;
     for (int i = 1; i <= 5; i++)
         q.push(i);
-    printf("%d %d\n", q.front(), q.back());
+    for (int i = 1; i <= 3; i++)
+        q.pop();
+    printf("%d\n", q.front());
     return 0;
 }
-
-// 3.queue常用函数实例
