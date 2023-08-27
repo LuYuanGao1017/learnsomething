c773c099a936db74fdd315982b316af860470114
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-27 09:16:12 +0800

    Update stack_use.cpp

--- an/chapater6/stack_use.cpp
+++ an/chapater6/stack_use.cpp
@@ -2,7 +2,7 @@
  * @Author: LynnGao
  * @Date: 2023-08-26 18:32:08
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-27 09:00:45
+ * @LastEditTime: 2023-08-27 09:15:31
  * @Description: 栈 后进先出
  * @FilePath: \learnsomething\an\chapater6\stack_use.cpp
  */
@@ -14,11 +14,27 @@
 #include <stdio.h>
 #include <stack>
 using namespace std;
+// int main()
+// {
+//     stack<int> st;
+//     for (int i = 1; i <= 5; i++)
+//         st.push(i);
+//     printf("%d\n", st.top());
+//     return 0;
+// }
+
+// 3. stack常用函数实例
+// (1) push()
+// (2) top()
+// (3) pop()
+
 int main()
 {
     stack<int> st;
     for (int i = 1; i <= 5; i++)
         st.push(i);
+    for (int i = 1; i <= 3; i++)
+        st.pop();
     printf("%d\n", st.top());
     return 0;
 }
