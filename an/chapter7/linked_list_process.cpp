38216295f447a353ff5988f0a50eec3724f27f79
Lynn Gao <91652776+LuYuanGao1017@users.noreply.github.com>
2023-08-30 09:59:01 +0800

    Update linked_list_process.cpp

--- an/chapter7/linked_list_process.cpp
+++ an/chapter7/linked_list_process.cpp
@@ -2,7 +2,7 @@
  * @Author: LynnGao
  * @Date: 2023-08-29 21:21:07
  * @LastEditors: LynnGao
- * @LastEditTime: 2023-08-30 09:57:54
+ * @LastEditTime: 2023-08-30 09:59:00
  * @Description:
  * @FilePath: \learnsomething\an\chapter7\linked_list_process.cpp
  */
@@ -19,93 +19,93 @@
 // (2)dekete运算符 对应new
 
 // 7.3.3 链表的基本操作
-// 1.创建链表
-#include <stdio.h>
-#include <stdlib.h>
-struct node
-{
-    int data;
-    node *next;
-};
+// // 1.创建链表
+// #include <stdio.h>
+// #include <stdlib.h>
+// struct node
+// {
+//     int data;
+//     node *next;
+// };
 
-node *create(int Array[])
-{
-    node *p, *pre, *head;
-    head = new node;
-    head->next = NULL;
-    pre = head;
-    for (int i = 0; i < 5; i++)
-    {
-        p = new node;
-        p->data = Array[i];
-        p->next = NULL;
-        pre->next = p;
-        pre = p;
-    }
-    return head;
-}
+// node *create(int Array[])
+// {
+//     node *p, *pre, *head;
+//     head = new node;
+//     head->next = NULL;
+//     pre = head;
+//     for (int i = 0; i < 5; i++)
+//     {
+//         p = new node;
+//         p->data = Array[i];
+//         p->next = NULL;
+//         pre->next = p;
+//         pre = p;
+//     }
+//     return head;
+// }
 
-int main()
-{
-    int Array[5] = {5, 3, 6, 1, 2};
-    node *L = create(Array);
-    L = L->next;
-    while (L != NULL)
-    {
-        printf("%d ", L->data);
-        L = L->next;
-    }
-    return 0;
-}
-// 2.查找元素
-// count 元素个数
-int search(node *head, int x)
-{
-    int count = 0;
-    node *p = head->next;
-    while (p != NULL)
-    {
-        if (p->data == x)
-        {
-            count++;
-        }
-        p = p->next;
-    }
-    return count;
-}
-// 3.插入元素
-void insert(node *head, int pos, int x)
-{
-    node *p = head;
-    for (int i = 0; i < pos - 1; i++)
-    {
-        p = p->next;
-    }
-    node *q = new node;
-    q->data = x;
-    q->next = p->next;
-    p->next = q;
-}
-// 4.删除元素
-void del(node *head, int x)
-{
-    node *p = head->next;
-    node *pre = head;
-    while (p != NULL)
-    {
-        if (p->data == x)
-        {
-            pre->next = p->next;
-            delete (p);
-            p = pre->next;
-        }
-        else
-        {
-            pre = p;
-            p = p->next;
-        }
-    }
-}
+// int main()
+// {
+//     int Array[5] = {5, 3, 6, 1, 2};
+//     node *L = create(Array);
+//     L = L->next;
+//     while (L != NULL)
+//     {
+//         printf("%d ", L->data);
+//         L = L->next;
+//     }
+//     return 0;
+// }
+// // 2.查找元素
+// // count 元素个数
+// int search(node *head, int x)
+// {
+//     int count = 0;
+//     node *p = head->next;
+//     while (p != NULL)
+//     {
+//         if (p->data == x)
+//         {
+//             count++;
+//         }
+//         p = p->next;
+//     }
+//     return count;
+// }
+// // 3.插入元素
+// void insert(node *head, int pos, int x)
+// {
+//     node *p = head;
+//     for (int i = 0; i < pos - 1; i++)
+//     {
+//         p = p->next;
+//     }
+//     node *q = new node;
+//     q->data = x;
+//     q->next = p->next;
+//     p->next = q;
+// }
+// // 4.删除元素
+// void del(node *head, int x)
+// {
+//     node *p = head->next;
+//     node *pre = head;
+//     while (p != NULL)
+//     {
+//         if (p->data == x)
+//         {
+//             pre->next = p->next;
+//             delete (p);
+//             p = pre->next;
+//         }
+//         else
+//         {
+//             pre = p;
+//             p = p->next;
+//         }
+//     }
+// }
 
 // 7.3.4 静态链表 原理 hash
 // 不需要头节点
